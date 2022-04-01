e6e952d7ef17ff0fa09e79a2eedf37a3394d5de1
Saulo Magalhães <saulo.magalhaes@outlook.com.br>
2022-03-31 23:09:45 -0300

    feat: Requisito 5

--- src/components/ExpenseForm/index.js
+++ src/components/ExpenseForm/index.js
@@ -1,18 +1,69 @@
 import React from 'react';
 import './styles.css';
-// import { connect } from 'react-redux';
-// import PropTypes from 'prop-types';
-// import trybe from './trybe.png';
-// import { requestLogin } from '../../actions';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Form extends React.Component {
   render() {
+    const { stateCurrencies } = this.props;
     return (
-      <form>
-        <input type="number" placeholder="Valor" />
+      <form className="form">
+        <label htmlFor="value">
+          Valor:
+          <input
+            id="value"
+            data-testid="value-input"
+            type="number"
+            placeholder="Valor"
+          />
+        </label>
+        <label htmlFor="select-currency">
+          Moeda:
+          <select id="select-currency">
+            {stateCurrencies.map((currency) => (
+              <option key={ currency } value={ currency }>
+                {currency}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="method">
+          Método de pagamento:
+          <select id="method" data-testid="method-input">
+            <option value="Dinheiro">Dinheiro</option>
+            <option value="Cartão de Crédito">Cartão de crédito</option>
+            <option value="Cartão de Débito">Cartão de débito</option>
+          </select>
+        </label>
+        <label htmlFor="category">
+          Categoria:
+          <select data-testid="tag-input">
+            <option value="Alimentação">Alimentação</option>
+            <option value="Lazer">Lazer</option>
+            <option value="Trabalho">Trabalho</option>
+            <option value="Transporte">Transporte</option>
+            <option value="Saúde">Saúde</option>
+          </select>
+        </label>
+        <label htmlFor="description">
+          Categoria:
+          <input
+            id="description"
+            data-testid="description-input"
+            type="text"
+            placeholder="Despesa"
+          />
+        </label>
       </form>
     );
   }
 }
+const mapStateToProps = (state) => ({
+  stateCurrencies: state.wallet.currencies,
+});
 
-export default Form;
+Form.propTypes = {
+  stateCurrencies: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default connect(mapStateToProps, null)(Form);
